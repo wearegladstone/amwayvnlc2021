125383b5fe3b1c779b60c1f93dcabbe8ea506b84
wearegladstone <81361622+wearegladstone@users.noreply.github.com>
2021-07-28 18:08:18 +0800

    Update amwayvnlc.js

--- amwayvnlc.js
+++ amwayvnlc.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
 
   //Main Page Button Clicks
   $('#mainAmwayMomentsButton').click(function(){
-    
-    
+
+
     transitionScenes($('#mainPage'), $('#amwayMomentsPage'));
 
   });
@@ -87,7 +87,6 @@ $(document).ready(function(){
 
   });
 
-
   $('#py2020Button').click(function(){
 
     transitionScenes($('#newAchieversRightBlock'), $('#py2020Block'));
@@ -98,6 +97,108 @@ $(document).ready(function(){
   $('#fcacaButton').click(function(){
 
     transitionScenes($('#beInspiredMainMenu'), $('#fcacaMenu'));
+
+  });
+
+  $('#py2019ABO1').click(function(){
+
+    openImage("images/Chan Koon Tin &Chai Foong.jpg");
+
+  });
+
+  $('#py2019ABO2').click(function(){
+
+    openVideo("https://player.vimeo.com/video/575724643");
+
+  });
+
+  $('#py2019ABO3').click(function(){
+
+    openVideo("https://player.vimeo.com/video/575724182");
+
+  });
+
+  $('#py2019ABO4').click(function(){
+
+    openVideo("https://player.vimeo.com/video/575724182");
+
+  });
+
+  $('#py2019ABO5').click(function(){
+
+    openVideo("https://player.vimeo.com/video/575724182");
+
+  });
+
+  $('#py2019ABO6').click(function(){
+
+    openVideo("https://player.vimeo.com/video/575724182");
+
+  });
+
+  $('#py2019ABO7').click(function(){
+
+    openVideo("https://player.vimeo.com/video/575724305");
+
+  });
+
+  $('#py2019ABO8').click(function(){
+
+    openVideo("https://player.vimeo.com/video/575725873");
+
+  });
+
+  $('#py2019ABO9').click(function(){
+
+    openVideo("https://player.vimeo.com/video/575725713");
+
+  });
+
+  $('#py2019ABO10').click(function(){
+
+    openVideo("https://player.vimeo.com/video/575724451");
+
+  });
+
+  $('#py2019ABO11').click(function(){
+
+    openVideo("https://player.vimeo.com/video/575723846");
+
+  });
+
+  $('#py2019ABO12').click(function(){
+
+    openVideo("https://player.vimeo.com/video/575725553");
+
+  });
+
+  $('#py2019ABO13').click(function(){
+
+    openVideo("https://player.vimeo.com/video/575724048");
+
+  });
+
+  $('#py2019ABO14').click(function(){
+
+    openVideo("https://player.vimeo.com/video/575725315");
+
+  });
+
+  $('#py2019ABO15').click(function(){
+
+    openImage("images/Patrick & Edna.jpg");
+
+  });
+
+  $('#py2019ABO16').click(function(){
+
+    openVideo("https://player.vimeo.com/video/575724798");
+
+  });
+
+  $('#py2019ABO17').click(function(){
+
+    openVideo("https://player.vimeo.com/video/575724966");
 
   });
 
@@ -117,39 +218,55 @@ $(document).ready(function(){
 
   //VNLC
 
+  function openVideo($url)
+  {
+
+    $('#video-iframe').attr('src',$url);
+    $('#videoBlock').fadeIn(500);
+
+  }
+
+  function openImage($imageSrc)
+  {
+
+      $('#imageBlockImage').attr('src', $imageSrc);
+      $('#imageBlock').fadeIn(500);
+
+  }
+
   function transitionScenes($from, $to)
   {
 
     $from.fadeOut(500, function(){
-      
+
       if($to.attr('id') == $("#beInspiredPage").attr('id'))
       {
         currentBeInspired = 0;
       }
-      
+
       if($to.attr('id') == $("#amwayMomentsPage").attr('id'))
       {
         $('#amwayMomentsEmbed').attr('src', wallsrc);
       }
-      
+
       if($from.attr('id') == $("#amwayMomentsPage").attr('id'))
       {
         $('#amwayMomentsEmbed').attr('src', "");
       }
-      
+
       if($('#bottomLeftLogo').css('opacity') == 0)
       {
-        
+
         $('#bottomLeftLogo').animate({opacity: 1}, 500);
-        
+
       }
-      
+
       $to.fadeIn(500)
-    
+
     });
-    
+
   }
-  
+
   //Video Block
   $('#videoCloseButton').click(function(){
 
@@ -157,7 +274,14 @@ $(document).ready(function(){
     $('#video-iframe').attr('src','');
 
   });
-  
+
+  //Image Block
+  $('#imageCloseButton').click(function(){
+
+    $('#imageBlock').fadeOut(500);
+
+  });
+
   //Vimeo Player
   //Intro Video Scene
   var introVideoiFrame = $('#introvideo-iframe');
